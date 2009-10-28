9bca4b5839e4f4ef0e222c75013229b2a11f48ea
Lyle Johnson <lyle@lylejohnson.name>
2009-10-28 07:16:56 -0700

    Corrected a typo in the regex for lib/fox16.rb

--- lib/fox16.rb
+++ lib/fox16.rb
@@ -1,4 +1,4 @@
-if RUBY_PLATFORM =~ (mswin|mingw)/i
+if RUBY_PLATFORM =~ /(mswin|mingw)/i
   require "#{RUBY_VERSION.sub(/\.\d+$/, '')}/fox16.so"
 elsif RUBY_PLATFORM =~ /darwin/
   require "fox16.bundle"
