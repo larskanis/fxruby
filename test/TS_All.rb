e755d22c0e4640c73f4fa6cd277733cf7fbffdf5
Lars Kanis <lars@greiz-reinsdorf.de>
2017-04-16 19:29:36 +0200

    Make TS_All independent to the pwd.

--- test/TS_All.rb
+++ test/TS_All.rb
@@ -2,8 +2,11 @@ require 'fox16'
 require 'test/unit'
 
 if __FILE__ == $0
-  $: << File.dirname(__FILE__)
-  Dir.glob("TC_*.rb").each do |testcase|
-    require "#{testcase}"
+  testdir = File.expand_path("..", __FILE__)
+  $: << testdir
+  Dir.chdir(testdir) do
+    Dir.glob("TC_*.rb").each do |testcase|
+      require "#{testcase}"
+    end
   end
 end
