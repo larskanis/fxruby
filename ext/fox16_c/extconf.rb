larskanis/fxruby

commit bb41dacd61a2db072a08af289648168b9208b335

Update workaround for Ubuntu to work with pkg-config
This was introduced in commit f9c2f9e45cfe6

ext/fox16_c/extconf.rb
@@ -241,7 +241,6 @@ def do_rake_compiler_setup
   $INCFLAGS << " -I#{File.join(File.dirname(__FILE__), 'include')}"
   if is_fxscintilla_build?
     $CPPFLAGS = $CPPFLAGS + " -DWITH_FXSCINTILLA -DHAVE_FOX_1_6"
-    find_library("fxscintilla", nil) || find_library(":libfxscintilla.so.19", nil) || ($libs = append_library($libs, "fxscintilla"))
   end
 
   checking_for("thread local variables") do
@@ -285,6 +284,15 @@ unless enable_config("win32-cross")
   unless fxscintilla_support_suppressed?
     checking_for "fxscintilla per pkg-config" do
       $autodetected_fxscintilla = pkg_config("fxscintilla")
+    end
+
+    unless $autodetected_fxscintilla
+      checking_for "fxscintilla on Ubuntu with missing libfxscintilla.so link" do
+        if find_library(":libfxscintilla.so.19", nil) && (cflags=pkg_config("fxscintilla", "cflags"))
+          $CXXFLAGS += " " + cflags
+          $autodetected_fxscintilla = true
+        end
+      end
     end
   end
   checking_for "fxscintilla build" do

commit aaceecdcbf236dd987693ab589b572ae7feec805

Fix cross build, broken since commit bb41dacd61

ext/fox16_c/extconf.rb
@@ -211,6 +211,7 @@ def do_rake_compiler_setup
     CONFIG['CC'] += "\nCXX=#{CONFIG['CXX']}" # Hack CXX into Makefile for cross compilation
     CONFIG['LDSHARED'].gsub!('gcc', 'g++') # ensure C++ linker is used, so that libstdc++ is linked static
     $LDFLAGS += " -s" # remove symbol table informations from shared lib
+    $libs = append_library($libs, "fxscintilla")
 
   elsif RUBY_PLATFORM =~ /mingw/
     $CFLAGS = $CFLAGS + " -I/usr/local/include"