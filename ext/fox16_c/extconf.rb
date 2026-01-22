63a6ac3efcf87fd72e73b22d0854e2be79b3a382
Lars Kanis <lars@greiz-reinsdorf.de>
2026-01-22 21:40:40 +0100

    Change download URI of libpng
    https://netcologne.dl.sourceforge.net isn't available currently.
    
    And update to 1.6.54

--- ext/fox16_c/extconf.rb
+++ ext/fox16_c/extconf.rb
@@ -23,9 +23,9 @@ end
 LIBZ_VERSION = ENV['LIBZ_VERSION'] || '1.3.1'
 LIBZ_SOURCE_URI = "http://zlib.net/fossils/zlib-#{LIBZ_VERSION}.tar.gz"
 
-LIBPNG_VERSION = ENV['LIBPNG_VERSION'] || '1.6.53'
-# LIBPNG_SOURCE_URI = "http://prdownloads.sourceforge.net/libpng/libpng-#{LIBPNG_VERSION}.tar.gz"
-LIBPNG_SOURCE_URI = "https://netcologne.dl.sourceforge.net/project/libpng/libpng16/#{LIBPNG_VERSION}/libpng-#{LIBPNG_VERSION}.tar.xz"
+LIBPNG_VERSION = ENV['LIBPNG_VERSION'] || '1.6.54'
+LIBPNG_SOURCE_URI = "http://prdownloads.sourceforge.net/libpng/libpng-#{LIBPNG_VERSION}.tar.gz"
+# LIBPNG_SOURCE_URI = "https://netcologne.dl.sourceforge.net/project/libpng/libpng16/#{LIBPNG_VERSION}/libpng-#{LIBPNG_VERSION}.tar.xz"
 
 # LIBJPEG_VERSION = ENV['LIBJPEG_VERSION'] || '9b'
 # LIBJPEG_SOURCE_URI = "http://www.ijg.org/files/jpegsrc.v#{LIBJPEG_VERSION}.tar.gz"
